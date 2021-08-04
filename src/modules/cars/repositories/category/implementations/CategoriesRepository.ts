d624a5e83f774a2a7a699420615b2a8301cd1311
demilsonpereira <demilson.d2102@gmail.com>
2021-08-04 14:48:24 -0400

    Resolvido o (QueryFailedError: invalid input syntax for type uuid), pois era por conta da falta das aspas {} no nome em CategoriesRepository na linha 30

--- src/modules/cars/repositories/category/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/category/implementations/CategoriesRepository.ts
@@ -27,7 +27,7 @@ class CategoriesRepository implements ICategoriesRepository {
   }
 
   async findByName(name: string): Promise<Category> {
-    const category = await this.repository.findOne(name);
+    const category = await this.repository.findOne({ name });
     return category;
   }
 }
